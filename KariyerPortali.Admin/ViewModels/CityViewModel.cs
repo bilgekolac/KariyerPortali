965ebd93d89c367df0c59328c8439b2629229b53
betulakgul <betul.akgul44@gmail.com>
2016-11-09 15:24:40 +0200

    edit

--- KariyerPortali.Admin/ViewModels/CityViewModel.cs
+++ KariyerPortali.Admin/ViewModels/CityViewModel.cs
@@ -1,18 +1,11 @@
-﻿using KariyerPortali.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
 namespace KariyerPortali.Admin.ViewModels
 {
-    public class CityViewModel
-    {
-        public int CityId { get; set; }
-        public string CityName { get; set; }
-       
-        public int CountryId { get; set; }
-        public Country Country { get; set; }
-    }
-} 
+	public class CityViewModel
+	{
+	}
+}
