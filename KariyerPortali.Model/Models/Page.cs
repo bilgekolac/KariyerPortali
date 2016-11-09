94b754b33764365c761a3c4890f4c8aa6f814f87
AliZalim <alizalimist@gmail.com>
2016-11-09 18:14:52 +0200

    page entity 2

--- KariyerPortali.Model/Models/Page.cs
+++ KariyerPortali.Model/Models/Page.cs
@@ -8,5 +8,10 @@ namespace KariyerPortali.Model.Models
 {
     class Page
     {
+        public int PageId { get; set; }
+        public string Title { get; set; }
+        public string MyProperty { get; set; }
+        public float Statistic { get; set; }
+        public DateTime CreateDate { get; set; }
     }
 }
