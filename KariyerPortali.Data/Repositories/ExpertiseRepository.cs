fe9d84dfdccec19e06e2ea944f907efd304ba87b
ArifeOzdemir <ozdemir.ariffe@gmail.com>
2016-11-10 14:53:53 +0200

    ex

--- KariyerPortali.Data/Repositories/ExpertiseRepository.cs
+++ KariyerPortali.Data/Repositories/ExpertiseRepository.cs
@@ -16,6 +16,66 @@ namespace KariyerPortali.Data.Repositories
     }
     public interface IExpertiseRepository : IRepository<Expertise>
     {
+        public IEnumerable<Expertise> Search(string search, int sortColumnIndex, string sortDirection, int displayStart, int displayLength, out int totalRecords, out int totalDisplayRecords)
+        {
+            search = search.Trim();
+            var searchWords = search.Split(' ');
+
+
+            var query = this.DbContext.Expertises.AsQueryable();
+            foreach (string sSearch in searchWords)
+            {
+                if (sSearch != null && sSearch != "")
+                {
+                    query = query.Where(l => l.ExpertiseName.Contains(sSearch));
+                }
+            }
+
+            var allExpertises = query;
+            IEnumerable<Expertise> filteredExpertises = allExpertises;
+            if (sortDirection == "asc")
+            {
+                switch (sortColumnIndex)
+                {
+                    case 1:
+                        filteredExpertises = filteredExpertises.OrderBy(l => l.ExpertiseId);
+                        break;
+                    case 2:
+                        filteredExpertises = filteredExpertises.OrderBy(l => l.ExpertiseName);
+                        break;
+
+                    default:
+                        filteredExpertises = filteredExpertises.OrderBy(l => l.ExpertiseId);
+                        break;
+                }
+            }
+            else
+            {
+                switch (sortColumnIndex)
+                {
+
+                    case 1:
+                        filteredExpertises = filteredExpertises.OrderByDescending(l => l.ExpertiseId);
+                        break;
+                    case 2:
+                        filteredExpertises = filteredExpertises.OrderByDescending(l => l.ExpertiseName);
+                        break;
+
+                    default:
+                        filteredExpertises = filteredExpertises.OrderByDescending(l => l.ExpertiseId);
+                        break;
+                }
+            }
+
+            var displayedExpertises = filteredExpertises.Skip(displayStart).Take(displayLength);
+            totalRecords = allExpertises.Count();
+            totalDisplayRecords = filteredExpertises.Count();
+            return displayedExpertises.ToList();
+        }
+        public interface IExpertiseRepository : IRepository<Expertise>
+        {
+            IEnumerable<Expertise> Search(string search, int sortColumnIndex, string sortDirection, int displayStart, int displayLength, out int totalRecords, out int totalDisplayRecords);
+        }
 
     }
 }
