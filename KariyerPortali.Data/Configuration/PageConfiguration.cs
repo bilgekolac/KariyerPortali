ee4b6fbaad34cb741c65eb21334006e24f0ea15d
AliZalim <alizalimist@gmail.com>
2016-11-10 16:03:03 +0200

    Page configuration zorunlu alanlar düzenlen di.

--- KariyerPortali.Data/Configuration/PageConfiguration.cs
+++ KariyerPortali.Data/Configuration/PageConfiguration.cs
@@ -15,6 +15,13 @@ namespace KariyerPortali.Data.Configuration
             ToTable("Table");
             HasKey<int>(p=>p.PageId);
             Property(p => p.Title).IsRequired().HasMaxLength(100);
+            Property(p => p.Slug).IsRequired().HasMaxLength(500);
+            Property(p => p.Body).IsRequired();
+            Property(p => p.ViewCount).IsRequired();
+            Property(p => p.CreatedBy).IsRequired().HasMaxLength(100);
+            Property(p => p.CreateDate).IsRequired();
+            Property(p => p.UptdatedBy).IsRequired();
+            Property(p => p.UpdateDate).IsRequired();
 
         }
     }
