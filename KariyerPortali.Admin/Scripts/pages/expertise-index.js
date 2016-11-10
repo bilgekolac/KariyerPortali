c783c1c970950a35d6a5b4f066a2ffea0f56f4de
ArifeOzdemir <ozdemir.ariffe@gmail.com>
2016-11-10 15:37:02 +0200

    c

--- KariyerPortali.Admin/Scripts/pages/expertise-index.js
+++ KariyerPortali.Admin/Scripts/pages/expertise-index.js
@@ -24,7 +24,7 @@
                 "next": "Sonraki",
                 "last": "Son",
                 "first": "İlk"
-            }
+            }, "sProcessing": "Yükleniyor..."
         },
 
         // Or you can use remote translation file
@@ -36,12 +36,14 @@
         // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js). 
         // So when dropdowns used the scrollable div should be removed. 
         //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
-
+        "bProcessing": true,
+        "bServerSide": true,
+        "sAjaxSource": "/Expertise/AjaxHandler",
         "bStateSave": true, // save datatable state(pagination, sort, etc) in cookie.
 
         "lengthMenu": [
             [5, 15, 20, -1],
-            [5, 15, 20, "All"] // change per page values here
+            [5, 15, 20, "Hepsi"] // change per page values here
         ],
         // set the initial value
         "pageLength": 5,
@@ -49,16 +51,26 @@
         "columnDefs": [
             {  // set default column settings
                 'orderable': false,
-                'targets': [0]
+                'searchable': false,
+                'targets': [0],
+                'render': function (data, type, row) {
+                    return '<label class="mt-checkbox mt-checkbox-single mt-checkbox-outline"><input type="checkbox" class="checkboxes" value="1" /><span></span></label>';
+                },
+
             },
-            {
-                "searchable": false,
-                "targets": [0]
-            },
-            {
-                "className": "dt-right",
-                //"targets": [2]
-            }
+             {
+                 'orderable': false,
+                 'searchable': false,
+                 'targets': [3],
+                 'render': function (data, type, row) {
+                     return '<div class="btn-group"><button class="btn btn-xs green dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">Eylemler<i class="fa fa-angle-down"></i></button>'
+                         + '<ul class="dropdown-menu" role="menu"><li><a href="/Expertise/Edit/' + row[0] + '"><i class="icon-note"></i> Düzenle</a</li><li>'
+                         + '<a href="/Expertise/Delete/' + row[0] + '" onclick="if (!confirm(\'Bu kaydı silmek istediğinize emin misiniz? Bu işlem geri alınamaz.\')) return false;"><i class="icon-ban"></i> Sil</a></li></ul></div>';
+                 }
+
+             }
+
+
         ],
         "order": [
             [1, "asc"]
