6b53b72e6ac83a82b66c4b55b1c3c791cfbea442
ArifeOzdemir <ozdemir.ariffe@gmail.com>
2016-11-10 14:38:51 +0200

    expertise

---
+++ KariyerPortali.Admin/ViewModels/ExpertiseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KariyerPortali.Admin.ViewModels
+{
+    public class ExpertiseViewModel
+    {
+        public int ExpertiseId { get; set; }
+        public string ExpertiseName { get; set; }
+    }
+}
