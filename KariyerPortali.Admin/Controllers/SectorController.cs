5c2cf937062f348371571dcd53cec8d2fa3b92b7
ezgiustebay <ebaran23>
2016-11-10 17:58:49 +0200

    sector

--- KariyerPortali.Admin/Controllers/SectorController.cs
+++ KariyerPortali.Admin/Controllers/SectorController.cs
@@ -1,4 +1,8 @@
-﻿using KariyerPortali.Service;
+﻿using AutoMapper;
+using KariyerPortali.Admin.Models;
+using KariyerPortali.Admin.ViewModels;
+using KariyerPortali.Model;
+using KariyerPortali.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,5 +29,43 @@ namespace KariyerPortali.Admin.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SectorFormViewModel sectorForm)
+        {
+            if (ModelState.IsValid)
+            {
+                var sector = Mapper.Map<SectorFormViewModel, Sector>(sectorForm);
+
+                sectorService.CreateSector(sector);
+                sectorService.SaveSector();
+                return RedirectToAction("Index");
+            }
+            return View(sectorForm);
+        }
+        public ActionResult AjaxHandler(jQueryDataTableParamModel param)
+        {
+            string sSearch = "";
+            if (param.sSearch != null) sSearch = param.sSearch;
+            var sortColumnIndex = Convert.ToInt32(Request["iSortCol_0"]);
+            var sortDirection = Request["sSortDir_0"]; // asc or desc
+            int iTotalRecords;
+            int iTotalDisplayRecords;
+            var displayedSectors = sectorService.Search(sSearch, sortColumnIndex, sortDirection, param.iDisplayStart, param.iDisplayLength, out iTotalRecords, out iTotalDisplayRecords);
+
+            var result = from e in displayedSectors
+                         select new[] { e.SectorId.ToString(), e.SectorId.ToString(), e.SectorName, string.Empty };
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = iTotalRecords,
+                iTotalDisplayRecords = iTotalDisplayRecords,
+                aaData = result.ToList()
+            },
+                JsonRequestBehavior.AllowGet);
+
+        }
+
     }
 }
