b87a737217afbeccbd88d98da6904b95fe6a141b
ArifeOzdemir <ozdemir.ariffe@gmail.com>
2016-11-11 18:31:24 +0200

    dvfgsd

--- KariyerPortali.Admin/Controllers/HomeController.cs
+++ KariyerPortali.Admin/Controllers/HomeController.cs
@@ -27,6 +27,19 @@ namespace KariyerPortali.Admin.Controllers
         public ActionResult Index()
         {
             ViewBag.JobApplicationCount = jobApplicationService.CountJobApplication();
+
+            ViewBag.JobApplicationMonth1 = jobApplicationService.GetJobApplicationCountByMonth(1);
+            ViewBag.JobApplicationMonth2 = jobApplicationService.GetJobApplicationCountByMonth(2);
+            ViewBag.JobApplicationMonth3 = jobApplicationService.GetJobApplicationCountByMonth(3);
+            ViewBag.JobApplicationMonth4 = jobApplicationService.GetJobApplicationCountByMonth(4);
+            ViewBag.JobApplicationMonth5 = jobApplicationService.GetJobApplicationCountByMonth(5);
+            ViewBag.JobApplicationMonth6 = jobApplicationService.GetJobApplicationCountByMonth(6);
+            ViewBag.JobApplicationMonth7 = jobApplicationService.GetJobApplicationCountByMonth(7);
+            ViewBag.JobApplicationMonth8 = jobApplicationService.GetJobApplicationCountByMonth(8);
+            ViewBag.JobApplicationMonth9 = jobApplicationService.GetJobApplicationCountByMonth(9);
+            ViewBag.JobApplicationMonth10 = jobApplicationService.GetJobApplicationCountByMonth(10);
+            ViewBag.JobApplicationMonth11 = jobApplicationService.GetJobApplicationCountByMonth(11);
+            ViewBag.JobApplicationMonth12 = jobApplicationService.GetJobApplicationCountByMonth(12);
             ViewBag.CandidateCount = candidateService.CountCandidate();
             ViewBag.ResumeCount = resumeService.CountResume();
             ViewBag.JobCount = jobService.CountJob();
