fcf4de99a840a3fa9649a902a02a787be30e7c06
Nexor <ertyeni@gmail.com>
2016-11-11 17:55:13 +0300

    as

--- KariyerPortali.Admin/Models/AccountViewModels.cs
+++ KariyerPortali.Admin/Models/AccountViewModels.cs
@@ -59,7 +59,7 @@ namespace KariyerPortali.Admin.Models
         [Display(Name = "Şifre")]
         public string Password { get; set; }
 
-        [Display(Name = "Tarayıcı hatırlasın mı")]
+        [Display(Name = "Beni hatırla")]
         public bool RememberMe { get; set; }
     }
 
@@ -91,14 +91,14 @@ namespace KariyerPortali.Admin.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Şifre en az 6 karakter olmalıdır.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Şifre")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Şifreyi Doğrula")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessage = "Şifre ve şifre doğrula uyuşmuyor.")]
         public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
