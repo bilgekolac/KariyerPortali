3ff0d3caf527e9ede9349cefc0436acaf23c6668
hakanyilmaz2057 <hakanyilmaz2057@gmail.com>
2016-11-14 16:07:20 +0200

    notification viev model oluşturuldu

---
+++ KariyerPortali.Admin/ViewModels/NotificationFormViewModel.cs
@@ -0,0 +1,23 @@
+﻿using KariyerPortali.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace KariyerPortali.Admin.ViewModels
+{
+    public class NotificationFormViewModel
+    {
+        public int NotificationId { get; set; }
+        [DisplayName("Mesaj")]
+        public string Message { get; set; }
+        [DisplayName("Detaylar")]
+        public string Details { get; set; }
+        [DisplayName("Bildirim Tarihi")]
+        public DateTime NotificationDate { get; set; }
+        [DisplayName("Bildirim Tipi")]
+        public virtual NotificationType NotificationType { get; set; }
+
+    }
+}
---
+++ KariyerPortali.Admin/ViewModels/NotificationViewModel.cs
@@ -0,0 +1,21 @@
+﻿using KariyerPortali.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KariyerPortali.Admin.ViewModels
+{
+    public class NotificationViewModel
+    {
+        public int NotificationId { get; set; }
+        public string Message { get; set; }
+        public string Details { get; set; }
+        public DateTime NotificationDate { get; set; }
+        public string Param1 { get; set; }
+        public string Param2 { get; set; }
+        public string Param3 { get; set; }
+
+        public virtual NotificationType NotificationType { get; set; }
+    }
+}
