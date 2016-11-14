4118f29062579087b01a6f290fb892df35adacfc
ecraany <eraycanbikmaz@gmail.com>
2016-11-14 17:41:06 +0200

    update

---
+++ KariyerPortali.Model/Models/Form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KariyerPortali.Model.Models
+{
+    public class Form
+    {
+        public int FormId { get; set; }
+
+        public string FormName { get; set; }
+
+        public string Emailbcc { get; set; }
+
+        public string Emailcc { get; set; }
+
+        public string Description { get; set; }
+
+        public string ClosingDescription { get; set; }
+
+        public virtual ICollection<FormInfo> FormInfos {get; set;}
+}
+}
