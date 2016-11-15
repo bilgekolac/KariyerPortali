bf48f34a5b8fa417734f80af61755b28ad444e40
ecraany <eraycanbikmaz@gmail.com>
2016-11-15 14:30:01 +0200

    update

--- KariyerPortali.Data/Repositories/FormInfoRepository.cs
+++ KariyerPortali.Data/Repositories/FormInfoRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KariyerPortali.Data.Infrastructure;
+using KariyerPortali.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace KariyerPortali.Data.Repositories
 {
-    class FormInfoRepository
+    public class FormInfoRepository : RepositoryBase<FormInfo>, IFormInfoRepository
     {
+        public FormInfoRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+    }
+    public interface IFormInfoRepository : IRepository<FormInfo>
+    {
+
     }
 }
