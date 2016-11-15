5836f5251bd4811fadf90770783e7d7edfe71669
Esraylmz <yilmaz-.-esra@hotmail.com>
2016-11-15 15:48:39 +0200

    redi

--- KariyerPortali.Service/RedirectService.cs
+++ KariyerPortali.Service/RedirectService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KariyerPortali.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,63 @@ using System.Threading.Tasks;
 
 namespace KariyerPortali.Service
 {
-    class RedirectService
-    {
-    }
+    //public interface IRedirectService
+    //{
+    //    IEnumerable<Redirect> GetCandidates();
+    //    Redirect GetCandidate(int id);
+    //    void CreateCandidate(Candidate candidate);
+    //    void UpdateCandidate(Candidate candidate);
+    //    void DeleteCandidate(Candidate candidate);
+    //    int CountCandidate();
+    //    void SaveCandidate();
+    //}
+    //public class RedirectService : IRedirectService
+    //{
+    //    private readonly ICandidateRepository candidateRepository;
+    //    private readonly IUnitOfWork unitOfWork;
+    //    public CandidateService(ICandidateRepository candidateRepository, IUnitOfWork unitOfWork)
+    //    {
+    //        this.candidateRepository = candidateRepository;
+    //        this.unitOfWork = unitOfWork;
+    //    }
+    //    #region ICandidateService Members
+    //    public IEnumerable<Candidate> Search(string search, int sortColumnIndex, string sortDirection, int displayStart, int displayLength, out int totalRecords, out int totalDisplayRecords)
+    //    {
+    //        var candidates = candidateRepository.Search(search, sortColumnIndex, sortDirection, displayStart, displayLength, out totalRecords, out totalDisplayRecords);
+
+    //        return candidates;
+    //    }
+    //    public IEnumerable<Candidate> GetCandidates()
+    //    {
+    //        var candidates = candidateRepository.GetAll();
+    //        return candidates;
+    //    }
+    //    public Candidate GetCandidate(int id)
+    //    {
+    //        var candidate = candidateRepository.GetById(id);
+    //        return candidate;
+    //    }
+    //    public void CreateCandidate(Candidate candidate)
+    //    {
+    //        candidateRepository.Add(candidate);
+    //    }
+    //    public void UpdateCandidate(Candidate candidate)
+    //    {
+    //        candidateRepository.Update(candidate);
+    //    }
+    //    public void DeleteCandidate(Candidate candidate)
+    //    {
+    //        candidateRepository.Delete(candidate);
+    //    }
+    //    public void SaveCandidate()
+    //    {
+    //        unitOfWork.Commit();
+    //    }
+    //    public int CountCandidate()
+    //    {
+    //        return candidateRepository.GetAll().Count();
+    //    }
+
+    //    #endregion
+    //}
 }
