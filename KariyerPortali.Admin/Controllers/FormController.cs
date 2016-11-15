5004d7d2a046b42cad1b47de1039c64b9d6adcf6
ecraany <eraycanbikmaz@gmail.com>
2016-11-15 15:42:05 +0200

    update

---
+++ KariyerPortali.Admin/Controllers/FormController.cs
@@ -0,0 +1,23 @@
+﻿using KariyerPortali.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KariyerPortali.Admin.Controllers
+{
+    public class FormController : BaseController
+    {
+        private readonly IFormService formService;
+        public FormController(IFormService formService)
+        {
+            this.formService = formService;
+        }
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
