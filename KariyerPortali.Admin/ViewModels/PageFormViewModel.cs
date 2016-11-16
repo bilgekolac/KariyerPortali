a8299bf3320d3ba943e52e8ffcde75328f9eebf8
AliZalim <alizalimist@gmail.com>
2016-11-16 17:42:21 +0200

    Entity ve viewlara seo özellikleri eklendi.

--- KariyerPortali.Admin/ViewModels/PageFormViewModel.cs
+++ KariyerPortali.Admin/ViewModels/PageFormViewModel.cs
@@ -42,5 +42,9 @@ namespace KariyerPortali.Admin.ViewModels
 
         [ForeignKey("ParentPageId")]
         public virtual Page ParentPage { get; set; }
+
+        public string SeoTitle { get; set; }
+        public string SeoDescription { get; set; }
+        public string KeyWords { get; set; }
     }
 }
