078f60aed01fa7af21a6545df4b01826337cc05e
Esraylmz <yilmaz-.-esra@hotmail.com>
2016-11-16 16:21:01 +0200

    redi

--- KariyerPortali.Admin/Controllers/RedirectController.cs
+++ KariyerPortali.Admin/Controllers/RedirectController.cs
@@ -20,5 +20,27 @@ namespace KariyerPortali.Admin.Controllers
             return View();
 
         }
+        //public ActionResult AjaxHandler(jQueryDataTableParamModel param)
+        //{
+        //    string sSearch = "";
+        //    if (param.sSearch != null) sSearch = param.sSearch;
+        //    var sortColumnIndex = Convert.ToInt32(Request["iSortCol_0"]);
+        //    var sortDirection = Request["sSortDir_0"]; // asc or desc
+        //    int iTotalRecords;
+        //    int iTotalDisplayRecords;
+        //    var displayedRedirects = redirectService.Search(sSearch, sortColumnIndex, sortDirection, param.iDisplayStart, param.iDisplayLength, out iTotalRecords, out iTotalDisplayRecords);
+
+        //    var result = from c in displayedRedirects
+        //                 select new[] { c.RedirectId.ToString(), c.RedirectName, c.OldUrl.ToString(), c.NewUrl.ToString(), c.RedirectType.ToString(), string.Empty };
+        //    return Json(new
+        //    {
+        //        sEcho = param.sEcho,
+        //        iTotalRecords = iTotalRecords,
+        //        iTotalDisplayRecords = iTotalDisplayRecords,
+        //        aaData = result.ToList()
+        //    },
+        //        JsonRequestBehavior.AllowGet);
+        //}
+
     }
 }
