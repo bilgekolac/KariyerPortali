a8df603a0ed34271aac214ebc4e22058322dc651
ecraany <eraycanbikmaz@gmail.com>
2016-11-16 18:14:23 +0200

    update

---
+++ KariyerPortali.Admin/Controllers/FormInfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KariyerPortali.Admin.Controllers
+{
+    public class FormInfoController : Controller
+    {
+        // GET: FormInfo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
