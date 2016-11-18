a670b55b13c60c06e922684401872bcab41aa097
hakanyilmaz2057 <hakanyilmaz2057@gmail.com>
2016-11-18 17:57:38 +0200

    create güncelleme (ali)

--- KariyerPortali.Service/NotificationService.cs
+++ KariyerPortali.Service/NotificationService.cs
@@ -15,7 +15,11 @@ namespace KariyerPortali.Service
         IEnumerable<Notification> Search(string search, int sortColumnIndex, string sortDirection, int displayStart, int displayLength, out int totalRecords, out int totalDisplayRecords);
         IEnumerable<Notification> GetLatestNotifications();
         Notification GetLatestNotification(int id);
-
+        void CreateNotification(Notification notification);
+        void UpdateNotification(Notification notification);
+        void DeleteNotification(Notification notification);
+        void SaveNotification();
+        int CountNotification();
     }
     public class NotificationService: INotificationService
     {
@@ -49,7 +53,26 @@ namespace KariyerPortali.Service
             var notifications = notificationRepository.Search(search, sortColumnIndex, sortDirection, displayStart, displayLength, out totalRecords, out totalDisplayRecords);
             return notifications;
         }
-
+        public void CreateNotification(Notification notification)
+        {
+            notificationRepository.Add(notification);
+        }
+        public void UpdateNotification(Notification notification)
+        {
+            notificationRepository.Update(notification);
+        }
+        public void DeleteNotification(Notification notification)
+        {
+            notificationRepository.Delete(notification);
+        }
+        public void SaveNotification()
+        {
+            unitOfWork.Commit();
+        }
+        public int CountNotification()
+        {
+            return notificationRepository.GetAll().Count();
+        }
         #endregion
     }
 }
