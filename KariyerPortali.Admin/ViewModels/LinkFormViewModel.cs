4e1b0412f119eae1fa1a8c7340d2fe8b7cab35fc
betulakgul <betul.akgul44@gmail.com>
2016-11-18 14:33:25 +0200

    link

--- KariyerPortali.Admin/ViewModels/LinkFormViewModel.cs
+++ KariyerPortali.Admin/ViewModels/LinkFormViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,14 @@ namespace KariyerPortali.Admin.ViewModels
 {
     public class LinkFormViewModel
     {
+        public int LinkId { get; set; }
+        [DisplayName("Link Adı")]
+        [Required(ErrorMessage = "Lütfen bir link adı giriniz.")]
+        public string LinkName { get; set; }
+        [DisplayName("URL")]
+        [Required(ErrorMessage = "Lütfen bir url giriniz.")]
+        public string Url { get; set; }
+        [DisplayName("Görünür")]
+        public bool Visible { get; set; }
     }
 }
